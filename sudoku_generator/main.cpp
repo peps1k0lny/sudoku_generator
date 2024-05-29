e68f15311bc92820426fa8113702c5dac3c0508c
aleksandrstrelcenko <alexstrelchen2006@gmail.com>
2024-05-29 17:50:14 +0300

    Initial Commit

---
+++ sudoku_generator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sudoku_generator
+//
+//  Created by Александр Стрельченко on 29.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
